a22e7a98333128d35be0dea371d99b19dd214084
Yi Li <licds@bc.edu>
2020-02-03 08:30:56 -0500

    Initial Commit

---
+++ wordGarden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordGarden
+//
+//  Created by Yi Li on 2/3/20.
+//  Copyright © 2020 Yi Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
